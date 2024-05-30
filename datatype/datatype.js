24d5e6181a8acd9fb009810f06b31db33adee725
KHUSHALMAHAJAN <khushalmahajan303@gmail.com>
2024-05-30 10:41:27 +0530

    add data type

---
+++ datatype/datatype.js
@@ -0,0 +1,21 @@
+let a = 8;
+console.log("a value of a is " +a)
+var b =9;
+console.log(a)
+let num = 10;
+let nu = null;
+let bu = true;
+let un = undefined;
+let bi = 10768678875;
+let str = "khushal manoj mahajan";
+console.log(num,nu,bu,un,bi,str)
+console.log(typeof num,typeof nu,typeof bu,typeof un,typeof bi,typeof str)
+let obj = {
+    stri : "mahajan khushal",
+    nu2 : "45"
+}
+console.log(obj)
+obj.salary = "5lakh/mon"
+console.log(obj)
+obj.salary = "60LPA"
+console.log(obj)
