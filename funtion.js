1d8d45394fc75c9f6dc94ed38eb903e559bc216e
KHUSHALMAHAJAN <khushalmahajan303@gmail.com>
2024-05-30 10:34:42 +0530

    add js

---
+++ funtion.js
@@ -0,0 +1,12 @@
+function fun1(a, b) {
+    console.log(a + b)
+}
+fun1(3, 3);
+let result = (a, b) => {
+    console.log("harry is best");
+    let y = a + b;
+    return y;
+}
+let c = result(4, 6);
+console.log(c);
+console.log(result)
---
+++ loop.js
@@ -0,0 +1,13 @@
+let obj = {
+    str: "khushal",
+    age: "18"
+}
+for (const key in obj) {
+
+    const element = obj[key];
+    console.log(element)
+
+}
+for (const c of "objt") {
+    console.log(c)
+}
